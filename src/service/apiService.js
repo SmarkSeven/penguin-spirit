a1969f47a5d6532489ae18c2f632e43dda96d815
SmarkSeven <wuziq@vip.qq.com>
2017-10-15 15:37:38 +0800

    change axios.defaults setting

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'http://www.qejl.vip/api'
 axios.defaults.timeout = 5000
-// axios.defaults.headers['Content-Type'] = 'text/plain;charset=UTF-8'
 
 axios.interceptors.request.use(
   config => {
