51a966f99e99dd706e0f1494ad67d2d0f121d72c
SmarkSeven <wuziq@vip.qq.com>
2017-10-16 13:23:09 +0800

    add eslint rule  allow 'no-return-await'

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,8 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    "no-useless-escape": 0
+    "no-useless-escape": 0,
+    "no-return-await": 0
 
   }
 }
