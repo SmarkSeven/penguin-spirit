4d3fb57e63ffcf94324229f86c30f2accb69540d
SmarkSeven <wuziq@vip.qq.com>
2017-10-28 18:37:44 +0800

    update docs

--- src/pages/purchas.vue
+++ src/pages/purchas.vue
@@ -159,8 +159,8 @@ export default {
 <style>
 .el-cascader,
 .el-select {
-  display: block;
-  position: static;
+  display: block !important;
+  /* position: static; */
 }
 .el-select-dropdown {
   max-width: 94%;
