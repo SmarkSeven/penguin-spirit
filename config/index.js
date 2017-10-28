9d56eb50496aca9325a751d8f8c20dee44dcb7d3
SmarkSeven <wuziq@vip.qq.com>
2017-10-28 18:28:51 +0800

    update docs

--- config/index.js
+++ config/index.js
@@ -10,8 +10,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'penguin-spirit/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/penguin-spirit',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
